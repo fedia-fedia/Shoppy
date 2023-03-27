07e6778c407091b5b86810454eec53cbbd583c86
Ahmed-jlidi <jlidi534@gmail.com>
2023-03-27 18:25:02 +0100

    lastchange

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,9 +7,13 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   constructor(private http:HttpClient) { }
-  url = "http://localhost:5000/api/signup"
+  url = "http://localhost:5000/api/v1/auth/signup"
+  url1 = "http://localhost:5000/api/v1/auth/login"
   signup(user:any):Observable<any>{
     return this.http.post<any>(this.url, user);
+  }
+  signin(user:any):Observable<any>{
+    return this.http.post<any>(this.url1, user);
   }
 
 }
